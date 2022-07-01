6802a747f2b7f925368a218fea96a22d5b721401
Daniel Skowroński <daniel@skowron.ski>
2022-07-01 23:29:08 +0200

    add two fields to schema

--- schema.py
+++ schema.py
@@ -11,13 +11,18 @@ class Plan(SAFRSBase, db.Model):
   start       = db.Column(db.Integer())
   end         = db.Column(db.Integer())
 
+  notes       = db.Column(db.String(1024))
+
 
 class Entry(SAFRSBase, db.Model):
   __tablename__ = "Entry"
   id          = db.Column(db.Integer, primary_key=True)
+  date        = db.Column(db.Integer())
   plan_id     = db.Column(db.Integer, db.ForeignKey("Plan.id"))
   plan        = db.relationship("Plan", back_populates="entries")
 
   person1     = db.Column(db.String(512))
-  person2     = db.Column(db.String(512))
+  person1_url = db.Column(db.String(1024))
+  person1     = db.Column(db.String(512))
+  person2_url = db.Column(db.String(1024))
   together    = db.Column(db.Boolean())
